43ba7cbc463311dc8b001cd2530960ba7672cf1e
ZhiQi <zhiqisim@gmail.com>
2019-01-14 11:37:36 +0800

    server.js

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var app = express();
 
 app.use(express.static(__dirname + '/views'));
 
+const port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Local Host 3000 listening!')
 });
